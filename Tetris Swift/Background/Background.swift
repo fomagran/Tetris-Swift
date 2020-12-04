e449ebc4722a86534ec9608954bf56a18e8f1f1e
Fomagran <fomagran6@naver.com>
2020-12-04 15:08:31 +0900

    아래 선 올라와 있는거 디버그

--- Tetris Swift/Background/Background.swift
+++ Tetris Swift/Background/Background.swift
@@ -78,28 +78,28 @@ class BackGround {
         leftLine.color = .darkGray
         leftLine.anchorPoint = CGPoint(x: 0.5, y: 1)
         leftLine.size = CGSize(width: 1, height: Variables.brickValue.brickSize * (col - 1))
-        leftLine.position = CGPoint(x: leftX, y: -topY)
+        leftLine.position = CGPoint(x: leftX, y: -topY+30)
         Variables.scene.addChild(leftLine)
         
         let rightLine = SKSpriteNode()
         rightLine.color = .darkGray
         rightLine.anchorPoint = CGPoint(x: 0.5, y: 1)
         rightLine.size = CGSize(width: 1, height: Variables.brickValue.brickSize * (col - 1))
-        rightLine.position = CGPoint(x: rightX, y: -topY)
+        rightLine.position = CGPoint(x: rightX, y: -topY+30)
         Variables.scene.addChild(rightLine)
         
         let bottomLine = SKSpriteNode()
         bottomLine.color = .darkGray
         bottomLine.anchorPoint = CGPoint(x: 0, y: 0.5)
         bottomLine.size = CGSize(width: rightX-leftX, height: 1)
-        bottomLine.position = CGPoint(x: leftX, y: -bottomY)
+        bottomLine.position = CGPoint(x: leftX, y: -bottomY+30)
         Variables.scene.addChild(bottomLine)
         
         let topLine = SKSpriteNode()
         topLine.color = .darkGray
         topLine.anchorPoint = CGPoint(x: 0, y: 0.5)
         topLine.size = CGSize(width: rightX-leftX, height: 1)
-        topLine.position = CGPoint(x: leftX, y: -topY)
+        topLine.position = CGPoint(x: leftX, y: -topY+30)
         Variables.scene.addChild(topLine)
     }
 }
